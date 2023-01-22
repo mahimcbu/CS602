26fc0e4f3b5f5f233efa188ffc8d2761c8dea01a
Mahim C <mahimc@bu.edu>
2023-01-22 09:40:49 -0500

    hw1 complete

--- Week1/CS602_HW1_Choudhury/hw1c.js
+++ Week1/CS602_HW1_Choudhury/hw1c.js
@@ -1,8 +1,48 @@
 const colors = require('colors');
+const { lookupByCityState } = require('./zipCodeModule_v2');
 
 const ZipCodeEmitter = require('./zipCodeEmitter').ZipCodeEmitter;
 
 const cities = new ZipCodeEmitter();
+
+console.log("Lookup by zip code: 02215".red);
+cities.on("lookupByZipCode", (args)=>{
+    console.log("Event lookupByZipCode raised!".blue, colors.blue(args));
+});
+
+cities.lookupByZipCode("02215");
+console.log("\n");
+
+console.log("Lookup by city, state: BOSTON, MA".red);
+cities.on("lookupByCityState",((arg1,arg2)=>{
+    console.log("Event lookupByCityState raised! (Handler1)\n".blue, arg1, arg2);
+}));
+console.log("\n");
+
+cities.on("lookupByCityState",((obj)=>{
+    const display = (data)=>{
+        for(var i = 0 ; i<data.length;i++){
+            console.log(colors.blue("  "+data[i].zip+" has population of "+data[i].pop));
+        }
+    }
+    console.log(colors.blue("\nEvent lookupByCityState raised! (Handler2)\n"+"City: "+obj.city+", State: "+obj.state));
+    display(obj.data)
+}));
+
+
+cities.lookupByCityState("BOSTON", "MA");
+console.log("\n");
+
+
+
+console.log("Get population by state: MA".red);
+
+
+cities.on("getPopulationByState", (args)=>{
+    console.log("Event getPopulationByState raised!\n".blue, colors.blue(args));
+});
+
+cities.getPopulationByState("MA");
 
 
 
--- Week1/CS602_HW1_Choudhury/zipCodeEmitter.js
+++ Week1/CS602_HW1_Choudhury/zipCodeEmitter.js
@@ -7,15 +7,54 @@ class ZipCodeEmitter  extends EventEmitter {
 	// member functions
 
 	lookupByZipCode(zip)  {
-	
-	}
+		let findMe = data.find((e) => {
+			if (e._id === zip) return true;
+			else return undefined});
+			this.emit('lookupByZipCode', findMe);	
+		// let found = false;
+		// let findMe = data.find((e) => {
+		// 	if (e._id === zip) this.emit('lookupByZipCode', e);
+		// 	//else this.emit('lookupByZipCode', undefined);
+		// });	
+		}
 
 	lookupByCityState(city, state)  {
-	
+		let found = false; //for display purpose
+		let myObj = {};
+		let myArray=[];
+		myObj["city"]= city;
+		myObj["state"]= state;
+		const place = data.filter(e =>{ //filter out the places for the given city and state
+			if (e.city === city && e.state === state){
+				found = true;
+				return true;
+			}
+			if (found === false){
+				myObj["data"]= [];
+			}
+		});
+	   const zip_pop= place.map(e=>{ //map the place array object to extract the zip and pop
+			let myzipAndpop = {} // object to store zip and pop and update within for loop
+			myzipAndpop["zip"]=e._id;
+			myzipAndpop["pop"]=e.pop;
+			myArray.push(myzipAndpop);
+			myObj["data"]= myArray;
+			
+		});
+		// console.log(myObj);
+		this.emit("lookupByCityState",myObj);
 	}
 
 	getPopulationByState(state) {
-	
+		let myObj2 = {};
+        myObj2["state"]= state;
+		const totalPop = data.reduce((total, e)=>{
+            if(state === e.state) return total + e.pop;
+            else return total;
+        },0);
+        myObj2["pop"]=totalPop;
+		this.emit("getPopulationByState",myObj2);
+        // console.log(myObj2);
 	}
 
 }
