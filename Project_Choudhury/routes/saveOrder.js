0c47c98dba95594d9635b833468d384f58efd28b
mahimcbu <mahimc@bu.edu>
2023-02-20 17:36:25 -0500

    Project with transaction

--- Project_Choudhury/routes/saveOrder.js
+++ Project_Choudhury/routes/saveOrder.js
@@ -1,45 +1,45 @@
-
 const customerDB = require('../customerOrderDB.js');
 const productDB = require('../productDB.js');
 
 const Order = customerDB.getModel();
 const Product = productDB.getModel();
 
-module.exports = function saveOrder(req, res, next) {
+module.exports = async function saveOrder(req, res, next) {
   const customerId = req.body.customerId;
   const total = req.body.total;
   const orderedProducts = JSON.parse(req.body.orderedProducts);
 
-  // save order
-  const order = new Order({
-    customerId,
-    orderedProducts,
-    total
-  });
+  const session = await Order.db.startSession(); // create a new session
 
-  order.save((error) => {
-    if (error) {
-      return next(error);
-    }
-
-    // update product quantity
-    const promises = orderedProducts.map(async (orderedProduct) => {
-      const product = await Product.findById(orderedProduct.productId);
-      if (!product) {
-        throw new Error('Product not found');
+  try {
+    await session.withTransaction(async () => {
+      // decrement product quantity and save order within the transaction
+      for (let i = 0; i < orderedProducts.length; i++) {
+        const orderedProduct = orderedProducts[i];
+        const product = await Product.findById(orderedProduct.productId).session(session);
+        if (!product) {
+          throw new Error('Product not found');
+        }
+        if (product.quantity < orderedProduct.quantity) {
+          throw new Error('Insufficient product quantity');
+        }
+        product.quantity -= orderedProduct.quantity;
+        await product.save();
       }
-      product.quantity -= orderedProduct.quantity;
-      await product.save();
-    });
 
-    Promise.all(promises)
-      .then(() => {
-        res.send({ status: 'success' });
-      })
-      .catch((error) => {
-        return next(error);
+      const order = new Order({
+        customerId,
+        orderedProducts,
+        total
       });
-  });
+
+      await order.save({ session });
+
+      res.send({ status: 'success' });
+    });
+  } catch (error) {
+    next(error);
+  } finally {
+    session.endSession(); // end the session when the transaction is complete
+  }
 };
-
-
